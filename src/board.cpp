6acb7c729bfde5306d7b3be67d1bf65b30d69baf
eru <eru160@protonmail.com>
2020-02-15 00:15:29 +0900

    .atじゃなくて[]でアクセスするようにした

--- src/board.cpp
+++ src/board.cpp
@@ -1,48 +1,38 @@
 #include "board.h"
 
-Board::Board()
-    : col(100)
-    , row(100)
-    , board(std::vector<std::vector<bool>>(row, std::vector<bool>(col, false))) {}
-
 Board::Board(int col, int row)
     : col(col)
     , row(row)
-    , board(std::vector<std::vector<bool>>(row, std::vector<bool>(col, false))) {}
+    // 番兵を含めるので2つ余分に取る(呼び出す側は番兵の存在を考慮しない)
+    , board(std::vector<std::vector<bool>>(row + 2, std::vector<bool>(col + 2, false))) {}
 
-int Board::arround(int x, int y) const {
-    int result = 0;
-    for (int dx = -1; dx <= 1; dx++) {
-        for (int dy = -1; dy <= 1; dy++) {
-            if (dx == 0 && dy == 0)
-                continue;
-            int nx = x + dx;
-            int ny = y + dy;
-            if (!(0 <= nx && nx < col && 0 <= ny && ny < row))
-                continue;
-            result += board.at(ny).at(nx);
-        }
-    }
-    return result;
-}
+bool Board::get(int x, int y) const { return board[y + 1][x + 1]; }
+
+void Board::set(int x, int y, bool cell) { board[y + 1][x + 1] = cell; }
 
 void Board::step() {
-    auto next = board;
+    std::vector<std::vector<bool>> next =
+        std::vector<std::vector<bool>>(row + 2, std::vector<bool>(col + 2, false));
     for (int y = 0; y < row; y++) {
         for (int x = 0; x < col; x++) {
-            switch (arround(x, y)) {
-            case 2:
-                break;
-            case 3:
-                next.at(y).at(x) = true;
-                break;
-            default:
-                next.at(y).at(x) = false;
-                break;
-            }
+            int cnt = 0;
+            cnt += get(x - 1, y - 1);
+            cnt += get(x - 1, y + 1);
+            cnt += get(x - 1, y);
+            cnt += get(x + 1, y - 1);
+            cnt += get(x + 1, y + 1);
+            cnt += get(x + 1, y);
+            cnt += get(x, y - 1);
+            cnt += get(x, y + 1);
+            if (cnt == 3 || (cnt == 2 && get(x, y)))
+                next[y + 1][x + 1] = true;
+            else
+                next[y + 1][x + 1] = false;
         }
     }
     board = next;
 }
 
-void Board::clear() { board = std::vector<std::vector<bool>>(row, std::vector<bool>(col, false)); }
+void Board::clear() {
+    board = std::vector<std::vector<bool>>(row + 2, std::vector<bool>(col + 2, false));
+}
--- src/board.h
+++ src/board.h
@@ -4,11 +4,11 @@
 
 class Board {
 public:
-    Board();
     Board(int col, int row);
 
-    bool get(int x, int y) const { return board.at(y).at(x); }
-    void set(int x, int y, bool cell) { board.at(y).at(x) = cell; }
+    // 0-indexedでアクセス
+    bool get(int x, int y) const;
+    void set(int x, int y, bool cell);
     int get_col() const { return col; }
     int get_row() const { return row; }
     bool check_bound(int x, int y) { return 0 <= x && x < col && 0 <= y && y < row; }
